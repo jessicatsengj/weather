566f95f33fed5c8ea8277adb67fc77b67aa14a91
jessicatsengj <jessica834945@yahoo.com.tw>
2015-05-22 11:23:47 +0800

    2

--- weather.js
+++ weather.js
@@ -1,16 +1,5 @@
-var datesContainer = $('.date');
-  $('#dateToday').css('font-weight', 'bolder').text('4 May 2015:: Mostly Cloudy');
-  $('#date1').css('font-weight', 'bolder').text('date1');
-  $('#date2').css('font-weight', 'bolder').text('date2');
-  $('#date3').css('font-weight', 'bolder').text('date3');
-
-var temperaturesContainer = $('temperatureOfDay');
-  $('#tp1').text('temperature1');
-  $('#tp2').text('temperature2');
-  $('#tp3').text('temperature3');
 
 var skycons = new Skycons();
-
   skycons.add("iconToday", Skycons.PARTLY_CLOUDY_DAY);
   skycons.add("icon1", Skycons.CLEAR_DAY);
   skycons.add("icon2", Skycons.CLOUDY);
@@ -25,9 +14,9 @@ var skycons = new Skycons();
 /*
 Get value from Bootstrap dropdown menu
 */
-$('#cities').on('click', function(){
-    alert($(this).text());
-});
+// $('#cities').on('click', function(){
+//     alert($(this).text());
+// });
 
 
 
